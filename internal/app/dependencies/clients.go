da390afbc4f7c6e804b92bad0e4504604349ae6a
eliasBaron <eliasnbaron@gmail.com>
2025-08-27 23:42:34 -0300

    feat: add PostgreSQL client and connection management functions

--- internal/app/dependencies/clients.go
+++ internal/app/dependencies/clients.go
@@ -8,6 +8,10 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
 )
 
 type MongoDBConfig struct {
@@ -56,4 +60,32 @@ func CloseMongoConnection(client *mongo.Client) error {
 	defer cancel()
 
 	return client.Disconnect(ctx)
+}
+
+func NewPostgreSQLClient() (*sql.DB, error) {
+	connStr := "user=postgres password=rootroot dbname=cpihub sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("error conectando a PostgreSQL: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("error verificando conexión a PostgreSQL: %w", err)
+	}
+
+	log.Printf("Conectado exitosamente a PostgreSQL")
+	return db, nil
+}
+
+func GetPostgreSQLDatabase() (*sql.DB, error) {
+	client, err := NewPostgreSQLClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func ClosePostgreSQLConnection(db *sql.DB) error {
+	return db.Close()
 }
